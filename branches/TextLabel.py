15264815860a2e15ef1354676477e0d0bfe327cc
dinnev@gmail.com <dinnev@gmail.com@599c6f36-b874-6429-4921-fb77a682882d>
2011-06-08 21:34:19 +0000

    comment

--- branches/TextLabel.py
+++ branches/TextLabel.py
@@ -7,6 +7,7 @@
 #	DESCRIPTION: User Interface text label component designed for applications using 
 #		graphical user interface compatible with and designed for the PYGAME library
 #	CONDITIONS: Requires BaseUIComponent.py to run
+#       PROJECT BY: http://www.sarconsrealm.org
 #	
 #	====================================================================================
 
